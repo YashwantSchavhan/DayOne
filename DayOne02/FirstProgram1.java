a6d21189fb31035962ad239e966267bc75d27da6
YashwantSchavhan <142770590+YashwantSchavhan@users.noreply.github.com>
2023-10-05 11:54:31 +0530

    Add files via upload

---
+++ DayOne02/FirstProgram1.java
@@ -0,0 +1,10 @@
+package DayOne02;
+
+public class FirstProgram1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ DayOne02/IdentifersDemo.java
@@ -0,0 +1,23 @@
+package DayOne02;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// variable declaration - Invalid Identifier Examples
+		/*int for = 13;//error becouse keyword cannot identifier
+		System.out.println("value of the number variable is :"+ for);*/
+
+		/*
+		 int number 2 = 13; // error because do not add space within identifier
+		 System.out.println("value of the number variable is :"+ number 2);
+		 */
+		// error because Identifier can not starts with @,#
+		// int @number3 = 10;
+		//Valid Identifier Examples
+		int _number = 20; //starts with $
+		System.out.println("value of the number variable is :" + _number);
+		String studentName="Yashwant";//use camelcase
+		System.out.println("value is :"+ studentName);
+	}
+
+}
